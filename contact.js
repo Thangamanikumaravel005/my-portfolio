7b40d256426b1dd1990126b4e26724a537128419
thangamani <lakshmithana620@gmail.com>
2026-06-09 09:50:03 +0530

    updated contact

--- contact.js
+++ contact.js
@@ -1,12 +1,24 @@
-document.getElementById("contactForm").addEventListener("submit", function(event){
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+
+    console.log("Form submitted");
 
     event.preventDefault();
 
     const name = document.getElementById("name").value;
+    const mobile = document.getElementById("mobile").value;
+    const message = document.getElementById("message").value;
+
+    console.log("Name:", name);
+    console.log("Mobile Number:", mobile);
+    console.log("Message:", message);
 
     document.getElementById("successMessage").innerHTML =
-    "Thank you, " + name + "! Your message has been submitted successfully.";
+        "Thank you, " + name + "! Your message has been submitted successfully.";
+
+    console.log("Success message displayed");
 
     document.getElementById("contactForm").reset();
+
+    console.log("Form reset completed");
 
 });
